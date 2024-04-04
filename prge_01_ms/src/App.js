b9e9631eb27b7872c77715a9df2f97b2e045ac19
PrecloweKotki <149382336+PrecloweKotki@users.noreply.github.com>
2024-04-04 13:12:01 +0200

    fix: added console log

--- prge_01_ms/src/App.js
+++ prge_01_ms/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const user_list = [
+  { imie: "Edwin" },
+  { imie: "Grzegorz" },
+  { imie: "Mirosław" },
+];
+
+console.log(user_list);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user_list.map((zolnierz) => {
+        return <div>{zolnierz.imie}</div>;
+      })}
     </div>
   );
 }
